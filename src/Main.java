2a796987ae6db6b1d99d0bb05aa4cca1368b3543
lorenamucheniski <lomucheniski25@gmail.com>
2023-01-29 16:13:34 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 4;
+
+        int soma = x + y;
+
+        System.out.println(soma);
+    }
+}
