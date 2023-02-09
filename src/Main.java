6df0a835f4530170a0a9d3e0d8ffa3154336258c
lorenamucheniski <lomucheniski25@gmail.com>
2023-02-09 14:14:16 -0300

    First class: Position

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import com.java.completo.udemy.Position;
+
 public class Main {
+
     public static void main(String[] args) {
-        int x = 5;
-        int y = 4;
 
-        int soma = x + y;
+        Position pos = new Position( 3, 5);
+        System.out.println(pos);
 
-        System.out.println(soma);
     }
 }
